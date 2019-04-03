37f0cfd60fa3e1bbf245ab095cf99fed9cec3baf
Hirokazu Hata <h.hata.ai.t@gmail.com>
2019-04-03 10:42:04 +0900

    Fix tmux config

--- .config/fish/conf.d/02_fzf.local.fish
+++ .config/fish/conf.d/02_fzf.local.fish
@@ -1,7 +1,7 @@
 # fzf config
 
 set -x FZF_DEFAULT_COMMAND 'rg --files --hidden --follow --glob "!.git/*"'
-set -x FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
+set -x FZF_CTRL_T_COMMAND 'rg --files --hidden --follow --glob "!.git/*"'
 set -x FZF_CTRL_T_OPTS '--preview "bat  --color=always --style=header,grid --line-range :100 {}"'
 
 # fe [FUZZY PATTERN] - Open the selected file with the default editor
