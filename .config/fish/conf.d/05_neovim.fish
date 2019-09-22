93f5606c52c21f0c8bb5805773e1623f3f333ba6
Hirokazu Hata <h.hata.ai.t@gmail.com>
2019-09-22 16:36:11 +0900

    Fix fish function for neovim

--- .config/fish/conf.d/05_neovim.fish
+++ .config/fish/conf.d/05_neovim.fish
@@ -1,9 +1,10 @@
-function nvim_install
+function neovim_install
   cd $HOME/ghq/github.com/h-michael/neovim
+  make clean
   make install CMAKE_BUILD_TYPE=RelWithDebInfo CMAKE_INSTALL_PREFIX=$HOME/.local
 end
 
-function nvim_ftest
+function neovim_ftest
   set TEST_FILE $argv[1]
   make functionaltest TEST_FILE=$TEST_FILE
 end
