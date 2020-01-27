e1b5d13bad2502101af77593f0c0f939fd5abbcf
Hirokazu Hata <h.hata.ai.t@gmail.com>
2020-01-27 09:19:55 +0900

    zsh: add zsh config

---
+++ .config/zsh/install_tools.zsh
@@ -0,0 +1,81 @@
+#!/bin/zsh
+
+function check_outdate {
+  git checkout master
+  git fetch origin
+  local UPSTREAM='@{u}'
+  local LOCAL=$(git rev-parse @)
+  local REMOTE=$(git rev-parse "$UPSTREAM")
+  local BASE=$(git merge-base @ "$UPSTREAM")
+
+  if [ $LOCAL = $REMOTE ]; then
+    echo "Up-to-date"
+    false
+  elif [ $LOCAL = $BASE ]; then
+    echo "Need to pull"
+    true
+  elif [ $REMOTE = $BASE ]; then
+    echo "Need to push"
+    false
+  else
+    echo "Diverged"
+    false
+  fi
+}
+
+function neovim_install {
+  cd $(ghq root)/github.com/h-michael/neovim
+  make install CMAKE_BUILD_TYPE=RelWithDebInfo CMAKE_INSTALL_PREFIX=$HOME/.local
+}
+
+function neovim_clean_install {
+  cd $(ghq root)/github.com/h-michael/neovim
+  make clean
+  make install CMAKE_BUILD_TYPE=RelWithDebInfo CMAKE_INSTALL_PREFIX=$HOME/.local
+}
+
+function neovim_ftest {
+  local TEST_FILE $argv[1]
+  local MIN_LOG_LEVEL 2
+  local LOG_DIR $HOME/.local/share/nvim/test/log
+  local NVIM_LOG_FILE $HOME/.local/share/nvim/test/.nvimlog
+  local NVIM_TEST_TRACE_LEVEL 2
+  # local BUSTED_ARGS '--coverage'
+  # local USE_LUACOV 1
+  make functionaltest TEST_FILE=$TEST_FILE \
+    BUSTED_PRG=$(which -a busted) \
+    MIN_LOG_LEVEL=2 \
+    LOG_DIR=$HOME/.local/share/nvim/test/log \
+    NVIM_TEST_TRACE_LEVEL=2
+    # BUSTED_ARGS='--coverage' \
+    # USE_LUACOV=1 \
+}
+
+function install_tmux {
+  cd $(ghq root)/github.com/tmux/tmux
+  if check_outdate; then
+    printf "input password"
+    read -s password
+
+    hub sync
+    sh autogen.sh
+    ./configure
+    make
+    echo $password | sudo -S make install
+  fi
+}
+
+function install_alacritty {
+  cd $(ghq root)/github.com/alacritty/alacritty
+  if check_outdate; then
+    printf "input password"
+    read -s password
+
+    hub sync
+    cargo build --release
+    echo $password | sudo -S rm /usr/local/bin/alacritty
+    echo $password | sudo -S cp target/release/alacritty /usr/local/bin/
+    echo $password | sudo -S mkdir -p /usr/local/share/man/man1
+    echo $password; gzip -c extra/alacritty.man | sudo -S tee /usr/local/share/man/man1/alacritty.1.gz 2> /dev/null
+  fi
+}
