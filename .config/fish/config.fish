eed01bedbb39c2f5b122df3783cd99452b6ecdd2
Hirokazu Hata <h.hata.ai.t@gmail.com>
2025-04-02 20:19:49 +0900

    Use mise instead of direnv

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -52,7 +52,6 @@ end
 
 fish_add_path $HOME/.local/bin
 
-direnv hook fish | source
 # https://mise.jdx.dev/dev-tools/shims.html#shims-vs-path
 #$HOME/.local/bin/mise activate fish | source
 eval ($HOME/.local/bin/mise activate fish --shims)
