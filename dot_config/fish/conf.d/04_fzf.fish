c23d21be2fde4640eb58c59a20831a0f96105f5b
Hirokazu Hata <h.hata.ai.t@gmail.com>
2025-09-03 04:24:33 +0900

    fish: update fzf option

--- dot_config/fish/conf.d/04_fzf.fish
+++ dot_config/fish/conf.d/04_fzf.fish
@@ -1,9 +1,31 @@
 # fzf config
 
 set -x FZF_DEFAULT_COMMAND 'rg --files --hidden --follow --glob "!.git/*"'
-set -x FZF_DEFAULT_OPTS '--reverse'
 set -x FZF_CTRL_T_COMMAND 'rg --files --hidden --follow --glob "!.git/*"'
 set -x FZF_CTRL_T_OPTS '--preview "bat  --color=always --style=header,grid --line-range :100 {}"'
+set -x FZF_DEFAULT_OPTS "--reverse \
+  --highlight-line \
+  --info=inline-right \
+  --ansi \
+  --layout=reverse \
+  --border=none \
+  --color=bg+:#283457 \
+  --color=bg:#16161e \
+  --color=border:#27a1b9 \
+  --color=fg:#c0caf5 \
+  --color=gutter:#16161e \
+  --color=header:#ff9e64 \
+  --color=hl+:#2ac3de \
+  --color=hl:#2ac3de \
+  --color=info:#545c7e \
+  --color=marker:#ff007c \
+  --color=pointer:#ff007c \
+  --color=prompt:#2ac3de \
+  --color=query:#c0caf5:regular \
+  --color=scrollbar:#27a1b9 \
+  --color=separator:#ff9e64 \
+  --color=spinner:#ff007c \
+"
 
 function fdh
   history | fzf | read -l item; and history delete --prefix "$item"
