53134c82f9e2233a835d6fd8206530807b39a98e
Hirokazu Hata <h.hata.ai.t@gmail.com>
2025-09-05 22:51:30 +0900

    nvim: introduce nvim-notify

--- dot_config/nvim/lua/plugins/appearance.lua
+++ dot_config/nvim/lua/plugins/appearance.lua
@@ -53,7 +53,7 @@ return {
             winbar = {},
           },
           always_divide_middle = true,
-          globalstatus = false,
+          globalstatus = true,
           refresh = {
             statusline = 1000,
             tabline = 1000,
@@ -86,6 +86,27 @@ return {
   },
   {
     'nvim-tree/nvim-web-devicons',
+  },
+  {
+    'nvim-mini/mini.icons',
+    version = false,
+  },
+  {
+    'rcarriga/nvim-notify',
+    config = function()
+      vim.notify = require("notify")
+      require("notify").setup({
+        fps = 60,
+        stages = "fade_in_slide_out",
+        timeout = 2000,
+        max_height = function() return math.floor(vim.o.lines * 0.40) end,
+        max_width = function() return math.floor(vim.o.columns * 0.40) end,
+      })
+      local severity = { "error", "warn", "info", "debug" }
+      vim.lsp.handlers["window/showMessage"] = function(_err, method, params, _client_id)
+          vim.notify(method.message, severity[params.type])
+      end
+    end,
   },
   {
     "catgoose/nvim-colorizer.lua",
