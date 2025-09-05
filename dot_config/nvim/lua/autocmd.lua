5af6faec9646541bd1aa0c5eedf6e58dfd90d1b5
Hirokazu Hata <h.hata.ai.t@gmail.com>
2025-09-06 00:39:17 +0900

    Apply StyLua

--- dot_config/nvim/lua/autocmd.lua
+++ dot_config/nvim/lua/autocmd.lua
@@ -1,78 +1,90 @@
 local api = vim.api
 
-api.nvim_create_augroup('MyAutoGroup', {})
-api.nvim_create_autocmd(
-  'WinEnter',
-  {
-    pattern = '*',
-    group = 'MyAutoGroup',
-    callback = function() vim.cmd [[ checktime ]] end,
-  }
-)
+api.nvim_create_augroup("MyAutoGroup", {})
+api.nvim_create_autocmd("WinEnter", {
+	pattern = "*",
+	group = "MyAutoGroup",
+	callback = function()
+		vim.cmd([[ checktime ]])
+	end,
+})
 
-api.nvim_create_augroup('AutoMkDir', {})
-api.nvim_create_autocmd(
-  'BufWritePre',
-  {
-    pattern = '*',
-    group = 'AutoMkDir',
-    desc = "Create new directories if directories doesn't exist",
-    callback = function()
-      -- http://vim-users.jp/2011/02/hack202/
-      local mk_dir_if_not_exist = function(dir)
-        if not (vim.fn.isdirectory(dir)) and vim.bo.buftype == "" and string.match(vim.fn.input(vim.fn.printf('"%s" does not exist. Create? [y/N]', dir)), '^y$') then
-          vim.fn.mkdir(vim.fn.iconv(dir, vim.opt.encoding:get(), vim.g.termencoding), 'p')
-        end
-      end
-      mk_dir_if_not_exist(vim.fn.expand('<afile>:p:h'))
-    end,
-  }
-)
+api.nvim_create_augroup("AutoMkDir", {})
+api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*",
+	group = "AutoMkDir",
+	desc = "Create new directories if directories doesn't exist",
+	callback = function()
+		-- http://vim-users.jp/2011/02/hack202/
+		local mk_dir_if_not_exist = function(dir)
+			if
+				not (vim.fn.isdirectory(dir))
+				and vim.bo.buftype == ""
+				and string.match(vim.fn.input(vim.fn.printf('"%s" does not exist. Create? [y/N]', dir)), "^y$")
+			then
+				vim.fn.mkdir(vim.fn.iconv(dir, vim.opt.encoding:get(), vim.g.termencoding), "p")
+			end
+		end
+		mk_dir_if_not_exist(vim.fn.expand("<afile>:p:h"))
+	end,
+})
 
 local function augroup(name)
-  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+	return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
 end
 
 vim.api.nvim_create_autocmd("VimEnter", {
-  group = augroup("autoupdate"),
-  callback = function()
-    if require("lazy.status").has_updates then
-      require("lazy").update({ show = false, })
-    end
-  end,
+	group = augroup("autoupdate"),
+	callback = function()
+		if require("lazy.status").has_updates then
+			require("lazy").update({ show = false })
+		end
+	end,
 })
 
-api.nvim_create_augroup('CargoTomlKeymaps', { clear = true })
-api.nvim_create_autocmd('BufEnter', {
-  group = 'CargoTomlKeymaps',
-  pattern = 'Cargo.toml',
-  callback = function()
-    local bufnr = api.nvim_get_current_buf()
-    local crates = require("crates")
-    local wk = require("which-key")
+api.nvim_create_augroup("CargoTomlKeymaps", { clear = true })
+api.nvim_create_autocmd("BufEnter", {
+	group = "CargoTomlKeymaps",
+	pattern = "Cargo.toml",
+	callback = function()
+		local bufnr = api.nvim_get_current_buf()
+		local crates = require("crates")
+		local wk = require("which-key")
 
-    wk.add({
-      { "<Leader>c", buffer = bufnr, group = "Cargo", remap = false },
-      { "<leader>cr", crates.reload, desc = "Reload", buffer = bufnr, remap = false },
-      { "<leader>ct", crates.toggle, desc = "Toggle", buffer = bufnr, remap = false },
-      { "<leader>cv", crates.show_versions_popup, desc = "Show versions", buffer = bufnr, remap = false },
-      { "<leader>cf", crates.show_features_popup, desc = "Show features", buffer = bufnr, remap = false },
-      { "<leader>cd", crates.show_dependencies_popup, desc = "Show dependencies", buffer = bufnr, remap = false },
+		wk.add({
+			{ "<Leader>c", buffer = bufnr, group = "Cargo", remap = false },
+			{ "<leader>cr", crates.reload, desc = "Reload", buffer = bufnr, remap = false },
+			{ "<leader>ct", crates.toggle, desc = "Toggle", buffer = bufnr, remap = false },
+			{ "<leader>cv", crates.show_versions_popup, desc = "Show versions", buffer = bufnr, remap = false },
+			{ "<leader>cf", crates.show_features_popup, desc = "Show features", buffer = bufnr, remap = false },
+			{ "<leader>cd", crates.show_dependencies_popup, desc = "Show dependencies", buffer = bufnr, remap = false },
 
-      { "<leader>cus", crates.update_crate, desc = "Update single crate", buffer = bufnr, remap = false },
-      { "<leader>cum", crates.update_crates, desc = "Update multiple crates", buffer = bufnr, remap = false },
-      { "<leader>ca", crates.update_all_crates, desc = "Update all crates", buffer = bufnr, remap = false },
-      { "<leader>cUs", crates.upgrade_crate, desc = "Upgrade single crate", buffer = bufnr, remap = false },
-      { "<leader>cUm", crates.upgrade_crates, desc = "Upgrade multiple crates", buffer = bufnr, remap = false },
-      { "<leader>cA", crates.upgrade_all_crates, desc = "Upgrade all crates", buffer = bufnr, remap = false },
+			{ "<leader>cus", crates.update_crate, desc = "Update single crate", buffer = bufnr, remap = false },
+			{ "<leader>cum", crates.update_crates, desc = "Update multiple crates", buffer = bufnr, remap = false },
+			{ "<leader>ca", crates.update_all_crates, desc = "Update all crates", buffer = bufnr, remap = false },
+			{ "<leader>cUs", crates.upgrade_crate, desc = "Upgrade single crate", buffer = bufnr, remap = false },
+			{ "<leader>cUm", crates.upgrade_crates, desc = "Upgrade multiple crates", buffer = bufnr, remap = false },
+			{ "<leader>cA", crates.upgrade_all_crates, desc = "Upgrade all crates", buffer = bufnr, remap = false },
 
-      { "<leader>cx", crates.expand_plain_crate_to_inline_table, desc = "Expand a plain crate declaration into an inline table", buffer = bufnr, remap = false },
-      { "<leader>cX", crates.extract_crate_into_table, desc = "Extract an crate declaration from a dependency section into a table", buffer = bufnr, remap = false },
+			{
+				"<leader>cx",
+				crates.expand_plain_crate_to_inline_table,
+				desc = "Expand a plain crate declaration into an inline table",
+				buffer = bufnr,
+				remap = false,
+			},
+			{
+				"<leader>cX",
+				crates.extract_crate_into_table,
+				desc = "Extract an crate declaration from a dependency section into a table",
+				buffer = bufnr,
+				remap = false,
+			},
 
-      { "<leader>cH", crates.open_homepage, desc = "Open homepage", buffer = bufnr, remap = false },
-      { "<leader>cR", crates.open_repository, desc = "Open repository", buffer = bufnr, remap = false },
-      { "<leader>cD", crates.open_documentation, desc = "Open documentation", buffer = bufnr, remap = false },
-      { "<leader>cC", crates.open_crates_io, desc = "Open crates.io", buffer = bufnr, remap = false },
-    })
-  end,
+			{ "<leader>cH", crates.open_homepage, desc = "Open homepage", buffer = bufnr, remap = false },
+			{ "<leader>cR", crates.open_repository, desc = "Open repository", buffer = bufnr, remap = false },
+			{ "<leader>cD", crates.open_documentation, desc = "Open documentation", buffer = bufnr, remap = false },
+			{ "<leader>cC", crates.open_crates_io, desc = "Open crates.io", buffer = bufnr, remap = false },
+		})
+	end,
 })
