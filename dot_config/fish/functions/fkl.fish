79598769cfaf5d0a8c58876d0cfdf5cf776009a5
Hirokazu Hata <h.hata.ai.t@gmail.com>
2025-11-07 01:51:18 +0900

    fish: add sccache config and improve fkl function
    - Add SCCACHE_CACHE_SIZE=50G for Rust compilation caching
    - Refactor fkl function to handle selection errors and pass additional args

--- dot_config/fish/functions/fkl.fish
+++ dot_config/fish/functions/fkl.fish
@@ -1,3 +1,11 @@
 function fkl -d "View logs from selected Kubernetes container"
-    eval (_fkp | awk '{print "kubectl logs -f " $1 " --container " $2}')
+    set -l selection (_fkp)
+    if test -z "$selection"
+        return 1
+    end
+
+    set -l pod (echo $selection | awk '{print $1}')
+    set -l container (echo $selection | awk '{print $2}')
+
+    kubectl logs -f $pod --container $container $argv
 end
