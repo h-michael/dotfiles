69f2bc59bebbcdeb10223283121749fa2eca8e82
Hirokazu Hata <h.hata.ai.t@gmail.com>
2025-11-12 20:09:09 +0900

    fish: remove chezmoi wrapper functions

--- dot_config/fish/functions/chezmoi-apply.fish
+++
@@ -1,4 +0,0 @@
-function chezmoi-apply -d "Apply both public and private chezmoi configurations"
-    chezmoi apply $argv --config ~/.config/chezmoi/chezmoi.toml --source ~/.local/share/dotfiles/chezmoi &&
-        chezmoi apply $argv --config ~/.config/chezmoi-private/chezmoi.toml --source ~/.local/share/dotfiles/chezmoi-private
-end
