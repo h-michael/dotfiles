309ed6e1dacdb8cfc5e644a6eb2559852bf75b2d
Hirokazu Hata <h.hata.ai.t@gmail.com>
2025-11-12 20:09:09 +0900

    fish: remove chezmoi wrapper functions

--- dot_config/fish/functions/chezmoi-diff.fish
+++
@@ -1,4 +0,0 @@
-function chezmoi-diff -d "Show diff for both public and private chezmoi configurations"
-    chezmoi diff $argv --config ~/.config/chezmoi/chezmoi.toml --source ~/.local/share/dotfiles/chezmoi &&
-        chezmoi diff $argv --config ~/.config/chezmoi-private/chezmoi.toml --source ~/.local/share/dotfiles/chezmoi-private
-end
