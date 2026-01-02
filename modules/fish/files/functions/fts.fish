d1d6612fc9d346ad3ad3eeb4ca3a5aaef74c0ee7
Hirokazu Hata <h.hata.ai.t@gmail.com>
2026-01-02 22:44:08 +0900

    fish: fix fts function

--- modules/fish/files/functions/fts.fish
+++ modules/fish/files/functions/fts.fish
@@ -6,7 +6,7 @@ function fts -d "Select tmux session and switch to it"
     --info=inline \
     --prompt="Select tmux session: " \
     --preview='bash -c "session_name=\$(echo {} | sed \"s/^[* ] *//\" | cut -d: -f1); echo \"Processes: \$(tmux list-panes -t \"\$session_name\" -F \"#{pane_current_command}\" 2>/dev/null | sort -u | paste -sd, -)\"; echo; tmux capture-pane -t \"\$session_name\" -e -p 2>/dev/null | bat --color=always --style=plain || echo \"Session preview unavailable\""' \
-    --preview-window=down:85%:wrap | awk '{print $2}' | cut -d: -f1)
+    --preview-window=down:85%:wrap | sed 's/^[* ]*//' | cut -d: -f1)
     if test -n "$session"
         if set -q TMUX
             tmux switch-client -t "$session"
