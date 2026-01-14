7d2f8b38ae7a387ae93add73a9c3e266022adb46
Hirokazu Hata <h.hata.ai.t@gmail.com>
2026-01-14 22:48:34 +0900

    Add merged indicator and exclude default branch from deletion
    - fgc: Show [merged] indicator for merged branches (except default branch)
    - fgdb: Exclude default branch from deletion candidates and show [merged] indicator

--- modules/fish/files/functions/fgc.fish
+++ modules/fish/files/functions/fgc.fish
@@ -1,5 +1,16 @@
 function fgc -d "git checkout selected branch"
-    set -l branch (git branch | fzf --reverse --prompt="Select branch: " | string trim | string replace -r '^\* ' '')
+    set -l default_branch (git symbolic-ref refs/remotes/origin/HEAD 2>/dev/null | string replace 'refs/remotes/origin/' '')
+    set -l merged_branches (git branch --merged $default_branch 2>/dev/null | string trim | string replace -r '^\* ' '')
+    set -l branch (git branch | string trim | string replace -r '^\* ' '' | while read -l b
+        if test "$b" = "$default_branch"
+            echo $b
+        else if contains $b $merged_branches
+            echo "$b [merged]"
+        else
+            echo $b
+        end
+    end | fzf --reverse --prompt="Select branch: " | string replace ' [merged]' '')
+
     if test -n "$branch"
         git checkout $branch
     end
--- modules/fish/files/functions/fgdb.fish
+++ modules/fish/files/functions/fgdb.fish
@@ -1,5 +1,17 @@
 function fgdb -d "git delete selected branch"
-    set -l branches (git branch | fzf --multi --prompt="Select branches to delete: " | string trim | string replace -r '^\* ' '')
+    set -l default_branch (git symbolic-ref refs/remotes/origin/HEAD 2>/dev/null | string replace 'refs/remotes/origin/' '')
+    set -l merged_branches (git branch --merged $default_branch 2>/dev/null | string trim | string replace -r '^\* ' '')
+    set -l branches (git branch | string trim | string replace -r '^\* ' '' | while read -l b
+        if test "$b" = "$default_branch"
+            continue
+        end
+        if contains $b $merged_branches
+            echo "$b [merged]"
+        else
+            echo $b
+        end
+    end | fzf --multi --prompt="Select branches to delete: " | string replace ' [merged]' '')
+
     if test -z "$branches"
         return 0
     end
