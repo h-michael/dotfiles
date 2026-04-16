1f7831cf178f0723d8b9a0d3e9be38256a0d0602
Hirokazu Hata <h.hata.ai.t@gmail.com>
2026-04-16 13:36:33 +0900

    Update tmux

--- overlays/tmux-master.nix
+++ overlays/tmux-master.nix
@@ -3,8 +3,8 @@ let
   tmuxMasterSrc = prev.fetchFromGitHub {
     owner = "tmux";
     repo = "tmux";
-    rev = "31d77e29b6c9fbb07d032018da78db3a8a38d979";
-    hash = "sha256-7Sc1KAVs0eSkTkbkGf/fN3ploC8ZOc4RgZPF+NoyGnQ=";
+    rev = "dd62c2f9467f975388f4a2701022752961bdb086";
+    hash = "sha256-4PK44+K5IuV8v/cw+HvZHfqzZEEvelG/Bm5HNL/ozNY=";
   };
 in
 {
