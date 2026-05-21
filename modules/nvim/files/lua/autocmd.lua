6e8a45a237974f0af46689e30b11e5bb3898800d
Hirokazu Hata <h.hata.ai.t@gmail.com>
2026-05-21 11:37:05 +0900

    Disable lazy.nvim auto-update on VimEnter
    Silent auto-update on startup overwrites the lockfile every launch,
    causing plugin versions to drift between machines. Switch to manual
    `:Lazy update` so updates are intentional.

--- modules/nvim/files/lua/autocmd.lua
+++ modules/nvim/files/lua/autocmd.lua
@@ -33,15 +33,15 @@ local function augroup(name)
 	return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
 end
 
-vim.api.nvim_create_autocmd("VimEnter", {
-	group = augroup("autoupdate"),
-	callback = function()
-		---@diagnostic disable-next-line: different-requires
-		if require("lazy.status").has_updates then
-			require("lazy").update({ show = false })
-		end
-	end,
-})
+-- vim.api.nvim_create_autocmd("VimEnter", {
+-- 	group = augroup("autoupdate"),
+-- 	callback = function()
+-- 		---@diagnostic disable-next-line: different-requires
+-- 		if require("lazy.status").has_updates then
+-- 			require("lazy").update({ show = false })
+-- 		end
+-- 	end,
+-- })
 
 api.nvim_create_augroup("CargoTomlKeymaps", { clear = true })
 api.nvim_create_autocmd("BufEnter", {
