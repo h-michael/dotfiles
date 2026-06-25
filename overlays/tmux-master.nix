1f126099b67d387ec60c8989b76f81d68a5049b6
Hirokazu Hata <h.hata.ai.t@gmail.com>
2026-06-25 13:31:55 +0900

    Bump tmux-master overlay to latest commit
    Update pinned tmux master to 6e6a434 (next-3.7).

--- overlays/tmux-master.nix
+++ overlays/tmux-master.nix
@@ -3,8 +3,8 @@ let
   tmuxMasterSrc = prev.fetchFromGitHub {
     owner = "tmux";
     repo = "tmux";
-    rev = "18ddda48d2e4cc401ae0ea018f175d38a90c9699";
-    hash = "sha256-9Mwe8DxNGUCwrcZBW5A1uydzRNrMP1uikRdEq38Kkb8=";
+    rev = "6e6a434a2d64a29855e071511d04f2d26ecf5ca5";
+    hash = "sha256-hIOTAKeF/oU4DDhItJRJ7SdTCSI4FqBaavLOhpwT7J0=";
   };
 in
 {
