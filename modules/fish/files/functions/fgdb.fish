7c622a757f6169b200f8eeba344cc11f3c6bdfc9
Hirokazu Hata <h.hata.ai.t@gmail.com>
2026-08-24 12:33:00 +0900

    Distinguish merged vs identical branches in fgdb, add colors

--- modules/fish/files/functions/fgdb.fish
+++ modules/fish/files/functions/fgdb.fish
@@ -4,6 +4,7 @@ function fgdb -d "git delete selected branch"
 
     set -l default_branch (git symbolic-ref refs/remotes/origin/HEAD 2>/dev/null | string replace 'refs/remotes/origin/' '')
     set -l merged_branches (git branch --merged $default_branch 2>/dev/null | string trim | string replace -r '^\* ' '')
+    set -l default_sha (git rev-parse $default_branch 2>/dev/null)
 
     set -l branches (git branch | string trim | string replace -r '^\* ' '' | while read -l b
         if test "$b" = "$default_branch"
@@ -11,19 +12,23 @@ function fgdb -d "git delete selected branch"
         end
 
         set -l is_merged (contains $b $merged_branches; and echo yes; or echo no)
+        set -l label ''
+        if test "$is_merged" = yes
+            if test (git rev-parse $b 2>/dev/null) = "$default_sha"
+                set label ' '(set_color cyan)'[no unique commits]'(set_color normal)
+            else
+                set label ' '(set_color yellow)'[merged]'(set_color normal)
+            end
+        end
 
         if set -q _flag_merged
             if test "$is_merged" = yes
-                echo "$b [merged]"
+                echo "$b$label"
             end
         else
-            if test "$is_merged" = yes
-                echo "$b [merged]"
-            else
-                echo $b
-            end
+            echo "$b$label"
         end
-    end | fzf --multi --prompt="Select branches to delete: " | string replace ' [merged]' '')
+    end | fzf --ansi --multi --prompt="Select branches to delete: " | string replace -ra '\x1b\[[0-9;]*m' '' | string replace -r ' \[(merged|no unique commits)\]$' '')
 
     if test -z "$branches"
         return 0
